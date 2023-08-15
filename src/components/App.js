d83122fc41f8f63bd6cf1faec47c18cdf498270a
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-08-15 06:18:36 +0000

    Completed

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,24 @@
 
 import React from "react";
 import './../styles/App.css';
+import { useState } from "react";
 
 const App = () => {
+  const [update,setUpdate]=useState('Tab 1')
   return (
     <div>
-        {/* Do not remove the main div */}
+        <ul>
+          <li onClick={()=>{
+           setUpdate('Tab 1')
+          }}>Tab 1</li>
+          <li onClick={()=>{
+           setUpdate('Tab 2')
+          }}>Tab 2</li>
+          <li onClick={()=>{
+           setUpdate('Tab 3')
+          }}>Tab 3</li>
+          <p>This is the content for {update}</p>
+        </ul>
     </div>
   )
 }
